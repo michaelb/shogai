f4c0c17f49dd96cc4df4143ffaa936b56f3df566
michaelb <michael.bleuez2@gmail.com>
2020-05-31 20:32:07 +0200

    correctly manage promoted piece possible moves

--- src/piece.rs
+++ src/piece.rs
@@ -55,7 +55,7 @@ impl Piece {
         //check position % 9 == (position + relat_mov%9)%9
 
         //also, whether the piece has to jump over other piece is not checked!
-        let possibles_moves: Vec<i32> = match &self.piecetype {
+        let mut possibles_moves: Vec<i32> = match &self.piecetype {
             PieceType::Pawn => vec![9],
             PieceType::King => vec![1, -1, 10, 8, 9, -9, -8, -10],
             PieceType::Rook => vec![
@@ -71,6 +71,22 @@ impl Piece {
             PieceType::Knight => vec![17, 19],
             PieceType::Lance => vec![9, 18, 27, 36, 45, 54, 63, 72],
         };
+
+        //manage promotion
+        if self.promoted {
+            if self.piecetype == PieceType::Pawn
+                || self.piecetype == PieceType::Lance
+                || self.piecetype == PieceType::Knight
+                || self.piecetype == PieceType::Lance
+            {
+                possibles_moves = vec![1, -1, 8, 9, 10, -9]
+            } else {
+                possibles_moves.append(vec![1, -1, 8, 9, 10, -8, -9, -10]);
+                possibles_moves.sort();
+                possibles_moves.dedup();
+            }
+        }
+
         let possibles_moves_colored;
         if self.color == Color::White {
             possibles_moves_colored = possibles_moves;
